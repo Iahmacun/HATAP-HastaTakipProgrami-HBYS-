00d8f5543fc9ac895e03903e3f7f868da958669e
Lahmacun <57403555+Iahmacun@users.noreply.github.com>
2024-07-09 01:33:35 +0300

    Update soket.js

--- soket.js
+++ soket.js
@@ -1,7 +1,7 @@
 import WebSocket from 'ws';
 import Message from './db.js';
 
-const wss = new WebSocket.Server({ port: 3030 });
+const wss = new WebSocket.Server({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', async function incoming(data) {
@@ -9,7 +9,7 @@ wss.on('connection', function connection(ws) {
       const messageData = JSON.parse(data);
       const { sender, recipient, content } = messageData;
       const message = await Message.create({ sender, recipient, content });
-      // Yeni mesajı tüm bağlı istemcilere yayınla
+      
       wss.clients.forEach(function each(client) {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
           client.send(JSON.stringify(message));
